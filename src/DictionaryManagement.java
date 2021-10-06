a60d1da24d556593768d897a6359610ee7f7e657
dragonthunder2 <cuong19052002@gmail.com>
2021-10-07 00:34:51 +0700

    CommandLine dictionary first version

--- src/DictionaryManagement.java
+++ src/DictionaryManagement.java
@@ -8,15 +8,32 @@ public class DictionaryManagement {
     static int n = 0;
 
     public static void insertFromFile() throws IOException {
+        ArrayList<String> wordsFromFile = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new FileReader("E:\\Java\\CommandLine\\Dictionary.txt"));
-
-        for (String currentWord = reader.readLine(); currentWord != null; currentWord = reader.readLine()) {
-            System.out.println(currentWord);
+        String line;
+        while ((line = reader.readLine()) != null) {
+            wordsFromFile.add(line);
         }
+        reader.close();
+
+        for (int i = 0; i < wordsFromFile.size(); i++) {
+            String[] str = wordsFromFile.get(i).split("\t");
+            Word result = new Word(str[0], str[1]);
+            Dictionary.WordList.add(result);
+            System.out.println(Dictionary.WordList.get(i).getWord_target() + "\t" + Dictionary.WordList.get(i).getWord_explain());
+        }
+
     }
 
     public static void dictionaryLookup() {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Search for word:");
+        String wordLookup = sc.nextLine();
+        for (int i = 0; i < Dictionary.WordList.size(); i++) {
+            if (wordLookup.equals(Dictionary.WordList.get(i).getWord_target())) {
+                System.out.println(Dictionary.WordList.get(i).getWord_explain());
+            }
+        }
     }
 
     public static void insertFromCommandline() {
