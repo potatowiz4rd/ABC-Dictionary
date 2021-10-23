c221df58b2550134044962a5fddfad024bba4333
dragonthunder2 <cuong19052002@gmail.com>
2021-10-23 07:22:44 +0700

    API Dictionary database added.

---
+++ src/InternetConnection.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class InternetConnection {
+    /**
+     * Send request to website.
+     * @param word Word requested for meaning;
+     * @return Meaning.
+     */
+    public String getOnlineData(String word) {
+        String data = "";
+        String decodedData = "";
+        try {
+            URL url = new URL("https://api.dictionaryapi.dev/api/v2/entries/en/" + word);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            if (conn.getResponseCode() == 200) {
+                InputStream in = conn.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String line = reader.readLine();
+                while (line != null) {
+                    data += line;
+                    line = reader.readLine();
+                }
+                reader.close();
+                JSONDecoder jd = new JSONDecoder();
+                decodedData = jd.Decoder(data);
+            } else {
+                decodedData = "error";
+                //System.out.println("Connection ERROR!");
+            }
+
+        } catch (Exception e) {
+            try {
+                //decodedData = "error";
+                System.out.println(e);
+            } catch (Exception e1) {
+                System.out.println(e1);
+            }
+        }
+        return decodedData;
+    }
+
+}
---
+++ src/JSONDecoder.java
@@ -0,0 +1,56 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+public class JSONDecoder {
+    public String Decoder(String data) {
+        String deData = "";
+        try {
+            Object obj = JSONValue.parse(data);
+
+            JSONArray ja = (JSONArray) obj;
+            JSONObject jo = (JSONObject) ja.get(0);
+            deData = deData + "Word: " + jo.get("word");
+
+            JSONArray phonetic = (JSONArray) jo.get("phonetics");
+            JSONObject text = (JSONObject) phonetic.get(0);
+            deData = deData + "\n Phonetic: " + text.get("text");
+
+            JSONArray meaning = (JSONArray) jo.get("meanings");
+
+            JSONObject PartOfSpeech = (JSONObject) meaning.get(0);
+            deData = deData + "\n Part Of Speech: " + PartOfSpeech.get("partOfSpeech");
+            JSONObject definitions = (JSONObject) meaning.get(0);
+            JSONArray definition = (JSONArray) definitions.get("definitions");
+            JSONObject def = (JSONObject) definition.get(0);
+            deData = deData + "\n + Definiton: " + def.get("definition");
+            deData = deData + "\n + Example: " + def.get("example");
+            JSONArray ex = (JSONArray) def.get("synonyms");
+            if (!(ex == null)) {
+                deData = deData + "\n + Synonyms: ";
+                for (int i = 0; i < ex.size(); i++) {
+                    deData = deData + "\n \t " + ex.get(i);
+                }
+            }
+
+            JSONObject PartOfSpeech2 = (JSONObject) meaning.get(1);
+            deData = deData + "\n Part Of Speech 2: " + PartOfSpeech2.get("partOfSpeech");
+            JSONObject definitions2 = (JSONObject) meaning.get(1);
+            JSONArray definition2 = (JSONArray) definitions2.get("definitions");
+            JSONObject def2 = (JSONObject) definition2.get(0);
+            deData = deData + "\n + Definiton: " + def2.get("definition");
+            deData = deData + "\n + Example: " + def2.get("example");
+            JSONArray ex2 = (JSONArray) def.get("synonyms");
+            if (!(ex2 == null)) {
+                deData = deData + "\n + Synonyms: ";
+                for (int i = 0; i < ex2.size(); i++) {
+                    deData = deData + "\n \t " + ex2.get(i);
+                }
+            }
+
+        } catch (Exception e) {
+            //deData = "error";
+        }
+        return deData;
+    }
+}
